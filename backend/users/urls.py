766d8834130b14f377a9a910efb420ecdca873f1
Ben <v.skidevelopers@gmail.com>
2022-08-09 12:20:57 +0300

    added react app

--- backend/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
-]
